831f867d7efee8699a0bd13f5e7aaaad784f3d5e
Mr DudE <90444865+Greatbotmaker@users.noreply.github.com>
2024-07-01 11:31:29 +0530

    Update bath save.py

--- TechVJ/save.py
+++ TechVJ/save.py
@@ -293,3 +293,78 @@ def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
     except:
         pass
         
+
+@Client.on_message(filters.command("batch"))
+async def batch_link(_, message):
+    user_id = message.chat.id    
+    lol = await chk_user(message, user_id)
+    if lol == 1:
+        return    
+        
+    start = await Client.ask(message.chat.id, text="Please send the start link.")
+    start_id = start.text
+    s = start_id.split("/")[-1]
+    cs = int(s)
+    
+    last = await Client.ask(message.chat.id, text="Please send the end link.")
+    last_id = last.text
+    l = last_id.split("/")[-1]
+    cl = int(l)
+
+    if cl - cs > 1000:
+        await Client.send_message(message.chat.id, "Only 1000 messages allowed in batch size... Make sure your start and end message have difference less than 1000")
+        return
+    
+    try:     
+        data = await db.get_data(user_id)
+        
+        if data and data.get("session"):
+            session = data.get("session")
+            try:
+                userbot = Client(":userbot:", api_id=API_ID, api_hash=API_HASH, session_string=session)
+                await userbot.start()                
+            except:
+                return await Client.send_message(message.chat.id, "Please generate a new session.")
+        else:
+            await Client.send_message(message.chat.id, "Please generate a session first.")
+
+        try:
+            users_loop[user_id] = True
+            
+            for i in range(int(s), int(l)):
+                if user_id in users_loop and users_loop[user_id]:
+                    msg = await Client.send_message(message.chat.id, "Processing!")
+                    try:
+                        x = start_id.split('/')
+                        y = x[:-1]
+                        result = '/'.join(y)
+                        url = f"{result}/{i}"
+                        link = get_link(url)
+                        await asyncio.sleep(5)
+                        await get_msg(userbot, user_id, msg.id, link, 0, message)
+                        sleep_msg = Client.send_message(message.chat.id, "Sleeping for 10 seconds to avoid flood...")
+                        await asyncio.sleep(8) # isko kam jyada kr lena bc
+                        await sleep_msg.delete()
+                        await sleep(2)
+                    except Exception as e:
+                        print(f"Error processing link {url}: {e}")
+                        continue
+                else:
+                    break
+        except Exception as e:
+            await Client.send_message(message.chat.id, f"Error: {str(e)}")
+                    
+    except FloodWait as fw:
+        await Client.send_message(message.chat.id, f'Try again after {fw.x} seconds due to floodwait from Telegram.')
+    except Exception as e:
+        await Client.send_message(message.chat.id, f"Error: {str(e)}")
+
+
+@Client.on_message(filters.command("cancel"))
+async def stop_batch(_, message):
+    user_id = message.chat.id
+    if user_id in users_loop:
+        users_loop[user_id] = False
+        await Client.send_message(message.chat.id, "Batch processing stopped.")
+    else:
+        await Client.send_message(message.chat.id, "No active batch processing to stop.")
